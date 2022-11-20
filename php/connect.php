616a5a485c03336920f02bd0be5430c52647a8bd
ehapinya <apinyasriyota@gmail.com>
2022-11-20 23:20:38 +0700

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?php
-$mysqli = new mysqli('localhost','root','','hotelbooking_draft');
-   if($mysqli->connect_errno){
+   $mysqli = new mysqli('localhost','root','','Anya-Booking');
+   if($mysqli->connect_errno) {
       echo $mysqli->connect_errno.": ".$mysqli->connect_error;
    }
- ?>
+?>
