0414f4ba8c498d490d5fc1e825a8af766c7124df
6322773829 <6322773829@g.siit.tu.ac.th>
2022-11-20 16:03:37 +0700

    หน้าregis login

--- php/profile.php
+++ php/profile.php
@@ -18,36 +18,23 @@
         </table>
     </header>
     <div class="navbar">
-        <?php      
-            $firstname = $_POST['firstname'];
-            $lastname = $_POST['lastname'];
-            $dob = $_POST['DOB'];
-            $phone_no = $_POST['phonenum'];
-            $email = $_POST['email'];
-            $citizen_id = $_POST['citizenID'];
+        <?php    
+           if(isset($_POST['submit']));
+            $username = $_POST['username'];
             $password = $_POST['passwd'];
-            $usergroup = $_POST['usergroup'];
-
-            //for test case that a user named Bob login in
-            if (($firstname == 'Bob') && ($password == '1234')){
-                $_SESSION["firstname"] = $firstname;
-            }
-
-            if ($_POST['usergroup'] == "Customer") {
-                echo("<a href='../php/customer_home.php'>&ensp;Home&ensp;</a>");
-            }
-            else if ($_POST['usergroup'] == "Hotel Owner") {
-                echo("<a href='../php/hotel_register.php'>&ensp;Add hotel&ensp;</a>");
-            }
-            else {
-                echo("<a href='../index.html'>&ensp;Home&ensp;</a>");
-            }
+            $q= "select * from customer, owner where customer.username='$username' or owner.username='$username'";
+                   $result=$mysqli->query($q);
+                   if(!$result){
+                       echo "Select failed. Error: ".$mysqli->error ;
+                       return false;
+                   }
         ?>
     </div>
 	<body class="shortpage">
 		<div id="content" class="form">
             <h1 style="font-size: 150%; margin-left:20px;"><b> Welcome to Anya Booking System</b> <br></h1>
-            <h3 class="center"> Welcome <?php echo $_POST["firstname"], " ",$_POST["lastname"], "!!!" ; ?><br> </h3>
+            <h3 class="center"> Welcome <?php 
+            $result=$mysqli->query($q);$row=$result->fetch_array(); echo $row['Firstname'], " ",$row['Lastname'], "!!!" ; ?><br> </h3>
             <br></br>
             <div class="center">
                 <img src="https://i.pinimg.com/736x/28/20/76/282076c6e9ff50f33c609c0f9a61da8a.jpg" height=240>
@@ -55,35 +42,38 @@
             <div class="center">
                 <h4>
                     <?php 
-                        echo "Name:",$_POST["firstname"], " ",$_POST["lastname"],"<br>";
-                        //--add "User Group: usergroup"-- 
-                        // echo "User Group:", " " ,$_POST["usergroup"],"<br>";
-                        //-- add "Email address: Email"-- 
-                        echo "Email address:", " " ,$_POST["email"],"<br>";
-                        //-- Find the gender and output "Gender: gender"-- 
-                        echo "Gender:", " ", $_POST["gender"],"<br>";
-                        echo "User group :", " ", $_POST['usergroup'], "<br>";
-                        
-                        
-                        
-                        //-- Find the age and output as "Age in years: age"--
-                        $byear = date('Y', strtotime($dob));
-                        echo "Age in years:", " " ,date("Y") - $byear,"<br>";
-                        //-- set default time zone--
-                        date_default_timezone_set("Asia/Bangkok");
-                        //-- print the login type as " Login time (local): time on date"-- 
-                        echo "Account was created on (local):"," ",date("h:i:sa")," on ",date("Y/m/d"), "<br><br>";
+                    $result=$mysqli->query($q); 
+                    $row=$result->fetch_array();
+                       $dob=$row['DOB'];
+                       echo "Name:",$row['Firstname'], " ",$row['Lastname'],"<br>";
+                       //--add "User Group: usergroup"-- 
+                       // echo "User Group:", " " ,$_POST["usergroup"],"<br>";
+                       //-- add "Email address: Email"-- 
+                       echo "Email address:", " " ,$row['Email'],"<br>";
+                       //-- Find the gender and output "Gender: gender"-- 
+                       echo "Gender:", " ", $row['gender_type'],"<br>";
+                       // echo "User group :", " ", $_POST['usergroup'], "<br>";
+                       
+                       
+                       
+                       //-- Find the age and output as "Age in years: age"--
+                       $byear = date('Y', strtotime($dob));
+                       echo "Age in years:", " " ,date("Y") - $byear,"<br>";
+                       //-- set default time zone--
+                       date_default_timezone_set("Asia/Bangkok");
+                       //-- print the login type as " Login time (local): time on date"-- 
+                       echo "Account was created on (local):"," ",date("h:i:sa")," on ",date("Y/m/d"), "<br><br>";
                         if ($_POST['usergroup'] == "Customer") {
-                            echo("<button onclick=\"location.href='customer_home.php'\" class=submit>&ensp;Home&ensp;</button>");
-                            echo("<button onclick=\"location.href='../index.html'\" class=submit>&ensp;Log out&ensp;</button>");
+                            echo("<button onclick=\"location.href='customer_home.php'\" id=submit>&ensp;Home&ensp;</button>");
+                            echo("<button onclick=\"location.href='../index.html'\" id=submit>&ensp;Log out&ensp;</button>");
                         }
                         else if ($_POST['usergroup'] == "Hotel Owner") {
-                            echo("<button onclick=\"location.href='hotel_register.php'\" class=submit>&ensp;Add hotel&ensp;</button>");
-                            echo("<button onclick=\"location.href='../index.html'\" class=submit>&ensp;Log out&ensp;</button>");
+                            echo("<button onclick=\"location.href='hotel_register.php'\" id=submit>&ensp;Add hotel&ensp;</button>");
+                            echo("<button onclick=\"location.href='../index.html'\" id=submit>&ensp;Log out&ensp;</button>");
                         }
                         else {
-                            echo("<button onclick=\"location.href='../index.html'\" class=submit>&ensp;Home&ensp;</button>");
-                        }
+                           echo("<button onclick=\"location.href='../index.html'\" id=submit>&ensp;Home&ensp;</button>");
+                       }
                     ?>
                 </h4> 
             </div>
