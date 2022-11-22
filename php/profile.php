ef344841272ff3160cd8cd7828c3ea8f3fafe703
ehapinya <apinyasriyota@gmail.com>
2022-11-22 19:49:56 +0700

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -21,39 +21,49 @@
     <div class="navbar">
         <?php    
             session_start();
-            
-            if ($_POST['username'] == "ADMIN" && $_POST['passwd'] == "0123") {
+            if ($_POST['username'] == "ADMIN" && $_POST['passwd'] == "0123" && (!isset($_POST['phonenum']))) {
                 header("location: admin_profile.php");
                 die();
             }
-
-            if (isset($_POST["username"])) {
-                $username = $_POST["username"];
-                $_SESSION["username"] = $username;
-            }
-            else {
-                $username = $_SESSION["username"];
-            }
             if (isset($_POST["usergroup"])) {
                 $usergroup = $_POST["usergroup"];
-                $_SESSION["usergroup"] = $usergroup;
+                $username = $_POST["username"];
+                $password = $_POST["passwd"];
                 if ($usergroup == "Hotel Owner") {
-                    header("location: owner_profile.php?username='$username'");
+                    $_SESSION["username"] = $username;
+                    $_SESSION["password"] = $password;
+                    header("location: owner_profile.php");
                     die();
                 }
             }
-            else {
-                $usergroup = $_SESSION["usergroup"];
+            if (isset($_POST["username"]) && (isset($_POST["passwd"]))) {
+                $username = $_POST["username"];
+                $password = $_POST["passwd"];
+                $q= "select * from customer where Username='$username' and Password='$password'";
+                $usergroup = $_POST["usergroup"];
+
             }
-            $q= "select * from customer where Username='$username'";
+            else if ((isset($_SESSION["username"])) && (isset($_SESSION["usergroup"]))) {
+                $username = $_SESSION["username"];
+                $usergroup = $_SESSION["usergroup"];
+                $q= "select * from customer where Username='$username'";
+            }
+        
+            
             $result=$mysqli->query($q);
-            if(!$result){
-                echo "Select failed. Error: ".$mysqli->error ;
-                return false;
+            if($result->num_rows == 0){
+                header("location: profile_register.php");
+                die();
             }   
+            
+            else {
+                $_SESSION["username"] = $username;
+                $_SESSION["usergroup"] = $usergroup;
+                echo "<a href='../php/customer_home.php'>&ensp;Home&ensp;</a>";
+                echo "<a href='customer_booking.php'>&ensp;Booking&ensp;</a>";
+            }
             $row=$result->fetch_array();
         ?>
-        <a href='../php/customer_home.php'>&ensp;Home&ensp;</a>
     </div>
 	<body class="shortpage">
 		<div id="content" class="form">
@@ -67,29 +77,25 @@
                 <h4>
                     <?php 
                     
-                       $dob=$row['DOB'];
-                       echo "Name:",$row['Firstname'], " ",$row['Lastname'],"<br>";
-                       //--add "User Group: usergroup"-- 
-                       echo "User Group:", " " ,$usergroup,"<br>";
-                       //-- add "Email address: Email"-- 
-                       echo "Username :", " ", $username, "<br>";
-                       echo "Email address:", " " ,$row['Email'],"<br>";
-                       //-- Find the gender and output "Gender: gender"-- 
-                       echo "Gender:", " ", $row['gender_type'],"<br>";
-                       // echo "User group :", " ", $_POST['usergroup'], "<br>";
-                       
-                       
-                       
-                       //-- Find the age and output as "Age in years: age"--
-                       $byear = date('Y', strtotime($dob));
-                       echo "Age in years:", " " ,date("Y") - $byear,"<br><br><br>";
-                    
+                        $dob=$row['DOB'];
+                        echo "Name:".$row['Firstname']. " ".$row['Lastname']."<br>";
+                        //--add "User Group: usergroup"-- 
+                        echo "User Group:", " " ,$usergroup,"<br>";
+                        //-- add "Email address: Email"-- 
+                        echo "Username :", " ", $username, "<br>";
+                        echo "Email address: ".$row['Email']."<br>";
+                        //-- Find the gender and output "Gender: gender"-- 
+                        echo "Gender: ". $row['gender_type']."<br>";                       
+                        
+                        
+                        //-- Find the age and output as "Age in years: age"--
+                        if ($dob != "") {
+                            $byear = date('Y', strtotime($dob));
+                            echo "Age in years:", " " ,date("Y") - $byear,"<br><br>";
+                        }
+                        
                         if ($usergroup == "Customer") {
                             echo("<button onclick=\"location.href='customer_home.php'\" class=submit>&ensp;Home&ensp;</button>");
-                            echo("<button onclick=\"location.href='../index.html'\" class=submit>&ensp;Log out&ensp;</button>");
-                        }
-                        else if ($usergroup == "Hotel Owner") {
-                            echo("<button onclick=\"location.href='hotel_register.php'\" class=submit>&ensp;Add hotel&ensp;</button>");
                             echo("<button onclick=\"location.href='../index.html'\" class=submit>&ensp;Log out&ensp;</button>");
                         }
                         else {
@@ -99,8 +105,8 @@
                 </h4> 
             </div>
 	    </div>
-        <!-- <div id="footer">
+        <div id="footer">
             CSS326 Section 1 Group 6
-        </div>   -->
+        </div>
     </body>
 </html>
